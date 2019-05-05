405c1c0adcaec6a4fb796e20f3dbba8c2fc37ed0
mpetkevic <miroslav.petkevic@gmail.com>
2019-05-05 12:14:17 +0300

    Updates

---
+++ precache-manifest.4e8adae9186064d4951f48c2079d3441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14646cb3c47e45db39745603cd3369a",
+    "url": "/nfq-fr4-hw/index.html"
+  },
+  {
+    "revision": "2dfec9cb96d7a83cced7",
+    "url": "/nfq-fr4-hw/static/css/main.92482c63.chunk.css"
+  },
+  {
+    "revision": "8890895d198efea45eba",
+    "url": "/nfq-fr4-hw/static/js/2.10fa05d7.chunk.js"
+  },
+  {
+    "revision": "2dfec9cb96d7a83cced7",
+    "url": "/nfq-fr4-hw/static/js/main.69466ae9.chunk.js"
+  },
+  {
+    "revision": "f260506425006ab45f97",
+    "url": "/nfq-fr4-hw/static/js/runtime~main.72e4b5c3.js"
+  }
+]);
